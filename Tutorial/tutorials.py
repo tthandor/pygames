fac5e0cca56148cd690279c72bb76e8d3954fd7e
tthandor <tthandor11@gmail.com>
2023-02-26 17:58:25 +0100

    move to constants

--- Tutorial/tutorials.py
+++ Tutorial/tutorials.py
@@ -1,14 +1,12 @@
 import pygame
 from sys import exit
+import constants as const
 
 pygame.init()
-screen = pygame.display.set_mode((794, 600))
+screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
 pygame.display.set_caption("Title of the game")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font("Text/Grand9K Pixel.ttf", 100)
-
-w = 100
-h = 200
+test_font = pygame.font.Font("Text/Grand9K Pixel.ttf", const.TEXT_SIZE)
 
 text_surface = test_font.render('Some title', True, 'White')
 
@@ -25,8 +23,8 @@ while True:
             exit()  # needed as when we close the game, we want to end the while loop as well
 
     screen.blit(background_surface, (0, 0))
-    bird_x += 4
-    if bird_x > 794:
+    bird_x += const.BIRD_SPEED
+    if bird_x > const.SCREEN_WIDTH:
         bird_x = 0
     screen.blit(bird_surface, (bird_x, bird_y))
     screen.blit(text_surface, (140, 150))
